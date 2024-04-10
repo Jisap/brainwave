b305828a8810597d8f6e31f2b53a62cffaea2b37
Jorge Matos <jisapjisap@gmail.com>
2024-04-10 17:32:51 +0200

    Pricing

---
+++ src/components/LeftLine.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LeftLine = () => {
+  return (
+    <div>LeftLine</div>
+  )
+}
+
+export default LeftLine
---
+++ src/components/RightLine.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RightLine = () => {
+  return (
+    <div>RightLine</div>
+  )
+}
+
+export default RightLine
